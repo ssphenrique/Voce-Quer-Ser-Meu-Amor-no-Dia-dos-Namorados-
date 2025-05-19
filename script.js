856f843637c6d0aa227195b67563116cc375222a
Pedro Henrique <135714305+ssphenrique@users.noreply.github.com>
2025-05-19 18:38:30 -0300

    Alteração nas frases

--- script.js
+++ script.js
@@ -53,17 +53,17 @@
     }
 })();
 */
-const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+const noMessages = [
+    "Tem certeza?",
+    "Tem mesmo certeza??",
+    "Você está absolutamente certa(o)?",
+    "Pookie, por favor...",
+    "Só pensa um pouquinho mais!",
+    "Se você disser não, eu vou ficar muito triste...",
+    "Eu vou ficar muito triste...",
+    "Eu vou ficar muito, muito, muito triste...",
+    "Ok, tudo bem, eu vou parar de perguntar...",
+    "Brincadeirinha, diz que sim, por favor! ❤️"
 ];
 
 let messageIndex = 0;
